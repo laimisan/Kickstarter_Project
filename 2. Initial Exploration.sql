3d8af93443594cb1983c6931f6b88864fab009c2
laimisan <98552550+laimisan@users.noreply.github.com>
2022-06-29 19:43:50 +0100

    Update 2. Initial Exploration.sql

--- 2. Initial Exploration.sql
+++ 2. Initial Exploration.sql
@@ -1,6 +1,8 @@
 -- DATA EXPLORATION Part 1
 
--- What state can a project be in?
+/* I am mostly interested in exploring trends in failed and successful projects.
+First let's see what other states can a project be in? */
+
 SELECT 
     state,
     COUNT(state) as project_count
@@ -9,12 +11,14 @@ FROM
 GROUP BY state
 ORDER BY project_count;
 
-/* My analysis will concern failed and successful projects. I will merge:
- 1. 'failed', 'cancelled', and ' suspended' into one 'failed' category,
- 2. 'live', 'undefined' into 'other' which will be excluded from analysis
- 3. 'successful' will be left as it is */
+/* There are six states in total, but I really only need two. Therefore I will:
+ 1. merge 'failed', 'cancelled', and ' suspended' into one 'failed' category,
+ 2. merge 'live' and 'undefined' into 'other' which will be excluded from analysis,
+ 3. leave 'successful' as it is. */
 
--- finding a total number of projects per country
+/* I will go ahead and do some light exploration and see what cleaning needs to be done.
+finding a total number of projects per country: */
+
 SELECT 
     country,
     COUNT(country) AS country_count
@@ -23,16 +27,16 @@ FROM
 GROUP BY country
 ORDER BY country_count;
 
-/* I can see from this that some data regarding countries was lost when data was scrapd.
-I will later use currency to populate missing data as much as possible */
+/* I can see from this that some data regarding countries was lost when data was scraped.
+I will later use currency to populate missing data as much as possible. */
 
--- finding a number of distinct countries in the dataset
+-- Finding the number of distinct countries in the dataset:
 
 SELECT
-	count(distinct country)
+    count(distinct country)
 FROM project_info;
 
--- finding how many projects there are per main category
+-- Finding how many projects there are per main category:
 
 SELECT 
     main_category,
@@ -42,13 +46,14 @@ FROM
 GROUP BY main_category
 ORDER BY project_count DESC;
 
--- finding the number of main categories
+-- Finding the number of main categories:
+
 SELECT 
     COUNT(DISTINCT main_category) as main_category_count
 FROM
     project_info;
     
--- finding the number of projects per sub-category
+-- Finding the number of projects per sub-category:
 
 SELECT 
     category,
@@ -58,16 +63,17 @@ FROM
 GROUP BY category
 ORDER BY project_count DESC;
 
--- finding how many sub-categories there are
+-- Finding how many sub-categories there are:
 
 SELECT 
     COUNT(DISTINCT category) as subcategory_count
 FROM
     project_info;
 
-/* clearly there are too many sub-categories for general overview. I will continue using main categories for now. */
+/* Clearly there are too many sub-categories for general overview. I will continue using main categories for now. */
 
--- checking the time period of the data
+-- Checking the time period of the data:
+
 SELECT 
     MIN(launched) as start_date,
     MAX(launched) as end_date
@@ -93,7 +99,7 @@ WHERE
     (YEAR(launched_date)) = '1970';
     
 /* Most of the data seems intact, but year is definitely wrong. I will keep the records,
-but will exclude the '1970' data when analysis is date-sensitive */
+but will exclude the '1970' data when analysis is date-sensitive. */
 
 SELECT 
     YEAR(pr.launched_date) as year_launched,
@@ -109,7 +115,7 @@ GROUP BY year_launched, year_deadline;
 /* As expected, there is a significant number of projects still live
 at the end of 2017 - beginning of 2018.*/
 
--- checking the types of currency used
+-- Checking the types of currency used:
 
 SELECT 
     currency,
@@ -119,23 +125,26 @@ FROM
 GROUP BY currency;
 
 /* 14 types of currency for 23 countries. Since I want to populate missing country data from
-the currency, I will check which currency is used in more than one country */
+the currency, I will check which currency is used in more than one country. */
 
 SELECT
-	currency,
+    currency,
     count(distinct(country)) as countries_per_currency
 FROM
-	pledge p
+    pledge p
 JOIN project_info pr on p.ID = pr.ID
 GROUP BY currency
 ORDER BY currency;
 
--- seems like all currencies except for Euros are safe to use to populate missing country data.
--- goal range in USD
+-- Seems like all currencies except for Euros are safe to use to populate missing country data.
+
+-- Goal range in USD:
 
 SELECT
     MIN(usd_goal_real) as min_goal_usd,
     AVG(usd_goal_real) as avg_goal_usd,
     MAX(usd_goal_real) as max_goal_usd
 FROM
-	pledge;
+    pledge;
+    
+-- I will now move to cleaning the data.
